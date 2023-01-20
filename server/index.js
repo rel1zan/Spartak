f6725c2b03bb0b8eba02da3d7f8b1917682aeafa
saller <relizanmail@gmail.com>
2023-01-20 18:32:09 +0300

    add modules and dependencies

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 const start = async () => {
     try{
