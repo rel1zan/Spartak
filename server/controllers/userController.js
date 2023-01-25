8a9ee1a485ddd7893d5d85d24ae23b35d3ba862c
saller <relizanmail@gmail.com>
2023-01-26 01:49:30 +0300

    add registration and login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,17 +1,47 @@
 const ApiError = require('../error/ApiError')
-class UserController {
-    async registration(req, res){
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
 
+const generateJwt = (id, phone_org, role) => {
+    return jwt.sign(
+        {id, phone_org, role},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'})
+}
+class UserController {
+    async registration(req, res, next){
+        const {name_org, person_org, address_org, phone_org, password, role} = req.body
+        if (!phone_org || !password){
+            return next(ApiError.badRequest('Некорректные данные'))
+        }
+        const candidate = await User.findOne({where: {name_org, person_org}})
+        if(candidate) {
+            return next(ApiError.badRequest('Пользователь существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({name_org, person_org, address_org, phone_org, password: hashPassword, role})
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJwt(user.id, user.phone_org, user.role)
+
+        return res.json(token)
     }
-    async login(req, res){
+    async login(req, res, next){
+        const {phone_org, password} = req.body
+        const user = await User.findOne({where:{phone_org}})
+        if (!user) {
+            return next(ApiError.internal("Пользователь не найден"))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal("Неверный пароль"))
+        }
+        const token = generateJwt(user.id, user.phone_org, user.role)
+        return res.json({token})
 
     }
     async check(req, res, next){
-        const {id} = req.query
-        if (!id) {
-            return next(ApiError.badRequest('Не задан ID'))
-        }
-        res.json(id)
+        
     }
 }
 
