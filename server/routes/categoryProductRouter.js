31b8bfd397950b1bcfa9913ca250e22034a5c296
saller <relizanmail@gmail.com>
2023-01-26 17:44:09 +0300

    add check role middleware

--- server/routes/categoryProductRouter.js
+++ server/routes/categoryProductRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const categoryController = require('../controllers/categoryController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', categoryController.create)
+router.post('/', checkRole('ADMIN'), categoryController.create)
 router.get('/', categoryController.getAll)
 
 module.exports = router
--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const productController = require('../controllers/productController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', productController.create)
+router.post('/', checkRole('ADMIN'), productController.create)
 router.get('/', productController.getAll)
 router.get('/:id', productController.getOne)
 router.delete('/', )
