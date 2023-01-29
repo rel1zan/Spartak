5dabff1def5b96dc75c41d31045e008e55b3f839
saller <relizanmail@gmail.com>
2023-01-29 17:05:19 +0300

    add routing pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 
 const App = () => {
     return (
-        <div>
-            Working
-        </div>
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
     );
 };
 
